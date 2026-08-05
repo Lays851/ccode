54ea4f81a2f0f35fd5551b755144371c995d03c5
Lays851 <2742682515@qq.com>
2026-08-05 20:27:13 +0800

    8月5日练习:链表demo,熟悉git提交流程

---
+++ 0805/git_practice.c
@@ -0,0 +1,23 @@
+// git 练习程序:打印一个链表
+// 今天的目的不是算法,是熟悉 git 的 add -> commit -> push 流程
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} Node;
+
+int main(void) {
+    Node *head = NULL;
+    for (int i = 1; i <= 5; i++) {
+        Node *p = malloc(sizeof(Node));
+        p->data = i;
+        p->next = head;
+        head = p;
+    }
+    for (Node *p = head; p; p = p->next)
+        printf("%d ", p->data);
+    printf("\n");
+    return 0;
+}
